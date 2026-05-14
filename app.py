edcf47bcf2e23b6da0d36a7ec5cc60f52b8ada14
ckyw0818 <ckyw0818@gmail.com>
2026-05-14 09:20:34 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+
+from bootstrap.container import Container
+
+def main():
+    app = Container().build_app()
+
+    port = int(os.environ.get("PORT", 7860))
+
+    app.launch(server_name="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    main()
